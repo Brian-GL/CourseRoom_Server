9cf9e62e8193c2ab0e7fd6392810691cab11868b
Brian GL <brian.gaytan4368@alumnos.udg.mx>
2022-05-01 15:58:28 -0500

    Migrados algoritmos inteligentes a courseroom analyzer

--- src/courseroom_server/CourseRoom_Server.java
+++ src/courseroom_server/CourseRoom_Server.java
@@ -4,7 +4,6 @@
  */
 package courseroom_server;
 
-import clases.Algoritmos;
 import com.formdev.flatlaf.FlatDarkLaf;
 import frames.CourseRoom_Server_Frame;
 import java.io.IOException;
@@ -20,7 +19,6 @@ public class CourseRoom_Server {
     public static void main(String args[]) throws ClassNotFoundException, UnsupportedLookAndFeelException, SQLException, IOException {
         FlatDarkLaf ui = new FlatDarkLaf();
         UIManager.setLookAndFeel(ui);
-        //var al = new Algoritmos();
         CourseRoom_Server_Frame principal_Frame = new CourseRoom_Server_Frame();
         principal_Frame.setVisible(true);
         
